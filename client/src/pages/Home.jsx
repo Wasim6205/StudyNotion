9cc2cfb86bb33464e2044a3635e9aa5ae84fb5b6
Wasim <wasimbhai6205@gmail.com>
2023-09-08 09:45:40 -0700

    section 1 more feature added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import { FaArrowRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import HighlightText from "../components/core/HomePage/HighlightText"
 import CTAButton from "../components/core/HomePage/Button"
+import Banner from "../assets/Images/banner.mp4"
+import CodeBlocks from "../components/core/HomePage/CodeBlocks";
 
 const Home = () => {
   return (
@@ -37,8 +39,78 @@ const Home = () => {
           </CTAButton>
         </div>
 
-        <div>
+        <div className="shadow-blue-200 mx-3 my-12">
+          <video
+          muted
+          loop
+          autoPlay
+          >
+            <source src={Banner} type="video/mp4"/>
+          </video>
+        </div>
 
+        {/* code section 1 */}
+        <div>
+          <CodeBlocks
+            position={"lg:flex-row"}
+            heading={
+              <div className="text-4xl font-semibold">
+                Unlock Your
+                <HighlightText text={"coding potential "}/>
+                with our online courses
+              </div>
+            }
+            subheading={"with our online coding course, with our online coding course with our online coding course with our online coding course with our online coding course"}
+            ctabtn1={
+              {
+                btnText: "try it yourself",
+                linkto: "/signup",
+                active:true,
+              }
+            }
+            ctabtn2={
+              {
+                btnText: "learn more",
+                linkto: "/login",
+                active:false,
+              }
+            }
+            codeblock={`<!doctype html>\n<html lang="en">\n<head>\n<meta charset="UTF-8" />\n<link rel="icon" type="image/svg+xml" href="/vite.svg" />\n<meta name="viewport" content="width=device-width, initial-scale=1.0" />\n<title>StudyNotion</title>\n</head\n<body>\n<div id="root"></div>\n<script type="module" src="/src/main.jsx"></script>\n</body>\n</html>
+            `}
+            codeColor={"text-yellow-25"}
+          />
+        </div>
+
+        {/* code section 2 */}
+        <div>
+          <CodeBlocks
+            position={"lg:flex-row-reverse"}
+            heading={
+              <div className="text-4xl font-semibold">
+                Unlock Your
+                <HighlightText text={"coding potential "}/>
+                with our online courses
+              </div>
+            }
+            subheading={"with our online coding course, with our online coding course with our online coding course with our online coding course with our online coding course"}
+            ctabtn1={
+              {
+                btnText: "try it yourself",
+                linkto: "/signup",
+                active:true,
+              }
+            }
+            ctabtn2={
+              {
+                btnText: "learn more",
+                linkto: "/login",
+                active:false,
+              }
+            }
+            codeblock={`<!doctype html>\n<html lang="en">\n<head>\n<meta charset="UTF-8" />\n<link rel="icon" type="image/svg+xml" href="/vite.svg" />\n<meta name="viewport" content="width=device-width, initial-scale=1.0" />\n<title>StudyNotion</title>\n</head\n<body>\n<div id="root"></div>\n<script type="module" src="/src/main.jsx"></script>\n</body>\n</html>
+            `}
+            codeColor={"text-yellow-25"}
+          />
         </div>
 
       </div>
